14ec3c9098d7188583415f6607f34ba6178c5f5e
Luiz Vinicius <viniciusadr244@gmail.com>
2024-10-08 10:24:32 -0300

    feat: feign log

---
+++ src/main/java/event/event_microservice/config/FeignLogConfiguration.java
@@ -0,0 +1,13 @@
+package event.event_microservice.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignLogConfiguration {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.HEADERS;
+    }
+}
